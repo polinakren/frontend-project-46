734096b719429a11c9cd72da727be0fc3d809eb7
Polina Kren <polina.kren8952@mail.ru>
2023-10-26 18:07:03 +0300

    Task-3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import path from 'path';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +10,12 @@ program
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const absolutePath1 = path.resolve(filepath1);
+    const absolutePath2 = path.resolve(filepath2);
+    const diff = genDiff(absolutePath1, absolutePath2);
+    console.log(diff);
+  });
 
 program.parse(process.argv);
