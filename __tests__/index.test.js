9b13826faf8ca55bf7ba6906789cb275c75047b6
Polina Kren <polina.kren8952@mail.ru>
2023-11-01 15:21:44 +0300

    Task-6: Added test for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,11 +10,22 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const expectedPath = getFixturePath('expected.txt');
 
-test('genDiff', () => {
+test('genDiff json', () => {
   const expected = readFileSync(expectedPath, 'utf-8');
 
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
+
+  console.log(genDiff(file1, file2));
+  console.log(expected);
+  expect(genDiff(file1, file2)).toEqual(expected);
+});
+
+test('genDiff yaml', () => {
+  const expected = readFileSync(expectedPath, 'utf-8');
+
+  const file1 = getFixturePath('file1.yaml');
+  const file2 = getFixturePath('file2.yaml');
 
   console.log(genDiff(file1, file2));
   console.log(expected);
