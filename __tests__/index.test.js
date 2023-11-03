131f72b8c452e61eceae2a50c1be39472ffdcc41
Polina Kren <polina.kren8952@mail.ru>
2023-11-03 14:15:01 +0300

    Task-9: edited tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,33 +1,39 @@
 import path from 'path';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
-import { test, expect } from '@jest/globals';
+import { test, expect, describe } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const expectedPath = getFixturePath('expected.txt');
+const expectedPathStylish = getFixturePath('expected_stylish.txt');
+const expectedPathPlain = getFixturePath('expected_plain.txt');
+const expectedPathJson = getFixturePath('expected_json.txt');
 
-test('genDiff json', () => {
-  const expected = readFileSync(expectedPath, 'utf-8');
+const expectedStylish = readFileSync(expectedPathStylish, 'utf-8');
+const expectedPlain = readFileSync(expectedPathPlain, 'utf-8');
+const expectedJson = readFileSync(expectedPathJson, 'utf-8');
 
-  const file1 = getFixturePath('file1.json');
-  const file2 = getFixturePath('file2.json');
+const file1Json = getFixturePath('file1.json');
+const file2Json = getFixturePath('file2.json');
 
-  console.log(genDiff(file1, file2));
-  console.log(expected);
-  expect(genDiff(file1, file2)).toEqual(expected);
-});
+const file1Yaml = getFixturePath('file1.yaml');
+const file2Yaml = getFixturePath('file2.yaml');
 
-test('genDiff yaml', () => {
-  const expected = readFileSync(expectedPath, 'utf-8');
+describe('genDiff test', () => {
+  test('yaml format', () => {
+    expect(genDiff(file1Yaml, file2Yaml)).toEqual(expectedStylish);
+    expect(genDiff(file1Yaml, file2Yaml, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(file1Yaml, file2Yaml, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(file1Yaml, file2Yaml, 'json')).toEqual(expectedJson);
+  });
 
-  const file1 = getFixturePath('file1.yaml');
-  const file2 = getFixturePath('file2.yaml');
-
-  console.log(genDiff(file1, file2));
-  console.log(expected);
-  expect(genDiff(file1, file2)).toEqual(expected);
+  test('json format', () => {
+    expect(genDiff(file1Json, file2Json)).toEqual(expectedStylish);
+    expect(genDiff(file1Json, file2Json, 'stylish')).toEqual(expectedStylish);
+    expect(genDiff(file1Json, file2Json, 'plain')).toEqual(expectedPlain);
+    expect(genDiff(file1Json, file2Json, 'json')).toEqual(expectedJson);
+  });
 });
